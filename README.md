5dedebb91a9883a45d5933dd61a679d6e4737e9a
IC1101 <malabananic1@gmail.com>
2021-12-01 00:18:43 +0000

    Add extras for atoll-user 10 QKQ1.200428.002 0390_202104211631 release-keys
    Signed-off-by: IC1101 <malabananic1@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## atoll-user 10 QKQ1.200428.002 0390_202104211631 release-keys
+- Manufacturer: realme
+- Platform: atoll
+- Codename: RMX2170
+- Brand: realme
+- Flavor: atoll-user
+- Release Version: 10
+- Id: QKQ1.200428.002
+- Incremental: 0390_202104211631
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: 
+- OTA version: RMX2170PU_11.A.39_0390_202104211631
+- Branch: RMX2170PU_11.A.39_0390_202104211631
+- Repo: realme_rmx2170_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/product/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/product/priv-app/Velvet/Velvet.apk
+rm -f system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/priv-app/SystemUI/SystemUI.apk.* 2>/dev/null >> system/system/priv-app/SystemUI/SystemUI.apk
+rm -f system/system/priv-app/SystemUI/SystemUI.apk.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null >> system/system/priv-app/OppoGallery2/OppoGallery2.apk
+rm -f system/system/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null
+cat system/system/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/priv-app/Settings/Settings.apk
+rm -f system/system/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null >> system/system/app/OppoCamera/OppoCamera.apk
+rm -f system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null
+cat system/system/apex/com.android.runtime.release.apex.* 2>/dev/null >> system/system/apex/com.android.runtime.release.apex
+rm -f system/system/apex/com.android.runtime.release.apex.* 2>/dev/null
+cat system/system/preload/OppoVideoEditor/OppoVideoEditor.apk.* 2>/dev/null >> system/system/preload/OppoVideoEditor/OppoVideoEditor.apk
+rm -f system/system/preload/OppoVideoEditor/OppoVideoEditor.apk.* 2>/dev/null
+cat oppo_product/app/Photos/Photos.apk.* 2>/dev/null >> oppo_product/app/Photos/Photos.apk
+rm -f oppo_product/app/Photos/Photos.apk.* 2>/dev/null
